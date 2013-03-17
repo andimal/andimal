01f8610a8992dde75fbba9c57bc2896d3fecef34
Andy Richardson <axrich@gmail.com>
2013-03-17 11:50:15 -0500

    equal heights on the work section

--- js/script.js
+++ js/script.js
@@ -18,3 +18,24 @@ $('.tile').hover(function() {
 });
 
 $('.tile-meta p').fitText();
+
+$('.row').each(function() {
+	var left	= $(this).children()[0],
+		right	= $(this).children()[1],
+		larger;
+
+	if($(left).outerHeight() < $(right).outerHeight()) {
+		larger = $(right).height();
+		$(left).height($(right).outerHeight());
+	} else {
+		larger = $(left).height();
+		$(right).height($(left).outerHeight());
+	}
+
+	$(this).children().each(function() {
+		$(this).find('.icon').css('line-height', larger + 'px');
+	});
+});
+
+
+
